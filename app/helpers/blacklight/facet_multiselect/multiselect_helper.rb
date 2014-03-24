6b06b087a72ff34638861f45bf7f6dd7050efef6
Chris Beer <cabeer@stanford.edu>
2014-03-24 15:33:49 -0400

    override #facet_partial_name to use the multiselect partial

--- app/helpers/blacklight/facet_multiselect/multiselect_helper.rb
+++ app/helpers/blacklight/facet_multiselect/multiselect_helper.rb
@@ -1,8 +1,11 @@
 require 'blacklight'
 module Blacklight::FacetMultiselect
   module MultiselectHelper
-    def facet_partial_name *args
-      super
+    def facet_partial_name display_facet
+      config = facet_configuration_for_field(display_facet.name)
+      name = config.try(:partial)
+      name ||= 'facet_multiselect' if config.multiselect
+      name ||= super
     end
   end
 end
